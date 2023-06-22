6c3737305c67100674495d0da2a6b2f96f19dd42
Dakota King <86220170+loribaltimore@users.noreply.github.com>
2023-06-22 09:25:21 -0700

    More updates to quiz UX

--- components/FullQuiz.js
+++ components/FullQuiz.js
@@ -2,39 +2,32 @@
 import Quiz from '../components/Quiz';
 import { useState } from 'react';
 import questions from '../util/questions';
-import { useSession } from 'next-auth/react';
 
 export default function FullQuiz({matched, setMatched}) {
-    const { data: session } = useSession();
     const [currentQuestion, setCurrentQuestion] = useState(0);
-    const [answers, setAnswers] = useState([]);
     let connectionAnswers = matched.allTriviaAnswers ? matched.allTriviaAnswers.me : undefined;
     const randomQuestions = connectionAnswers || questions.sort(() => Math.random() - 0.5).slice(0, 6);
-
-    const handleClick = async () => {
-        await fetch('/api/user/connections/quiz', {
-            method: 'POST',
-            body: JSON.stringify({
-                answers,
-                activeUserId: session.userId,
-                connectionId: matched.currentUser._id
-            }),
-            headers: {
-                'Content-Type': 'application/json',
-            }
-        }).then(async data => {
-            setMatched(false);
-        }).catch(err => console.log(err));
+    let question = false;
+    let answers;
+    let chosen;
+    
+    if (randomQuestions[currentQuestion]) {
+        question = randomQuestions[currentQuestion].question;
+        answers = randomQuestions[currentQuestion].answers;
+        chosen = randomQuestions[currentQuestion].chosen;
     };
+
     return (
         <div>
             {
-                    <Quiz query={randomQuestions[currentQuestion] || false}
-                        setCurrentQuestion={setCurrentQuestion}
-                        currentQuestion={currentQuestion}
-                        setAnswers={setAnswers}
-                        allChosen={answers}
-                        matchedName={matched.currentUser.name}
+                <Quiz setCurrentQuestion={setCurrentQuestion}
+                    chosen={chosen}
+                    currentQuestion={currentQuestion}
+                    randomQuestions={randomQuestions}
+                    matched={matched}
+                    setMatched={setMatched}
+                    question={question || false}
+                    possibleAnswers={answers}
                         />
             }
         </div>
--- components/Quiz.js
+++ components/Quiz.js
@@ -1,12 +1,34 @@
 "use client"
 import { useState } from 'react';
+import { useSession } from 'next-auth/react';
 
-export default function Quiz({ query, setCurrentQuestion, setAnswers, matchedName, allChosen}) {
-    const { question, answers, chosen } = query;
+export default function Quiz({randomQuestions, currentQuestion, setCurrentQuestion, question, possibleAnswers, chosen}) {
+    const { data: session } = useSession();
     const [isAnswered, setIsAnswered] = useState(false);
+    const [answers, setAnswers] = useState([]);
     const [showChosen, setShowChosen] = useState(false);
     const [isClicked, setIsClicked] = useState(undefined);
 
+    const questions = randomQuestions.map(question => question.question);
+    console.log(questions);
+    console.log(possibleAnswers)
+
+    const handleSubmit = async () => {
+        await fetch('/api/user/connections/quiz', {
+            method: 'POST',
+            body: JSON.stringify({
+                answers,
+                activeUserId: session.userId,
+                connectionId: matched.currentUser._id
+            }),
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        }).then(async data => {
+            setMatched(false);
+        }).catch(err => console.log(err));
+    };
+console.log(currentQuestion)
     const handleClick = (event) => {
         setShowChosen(true);
         setIsClicked(event.target.innerText);
@@ -20,20 +42,19 @@ export default function Quiz({ query, setCurrentQuestion, setAnswers, matchedNam
             }, 1000)
         },  1750);
     }
-    console.log(allChosen)
     return (
            <div className={`m-auto mt-28 items-center overflow-hidden bg-white px-4 pb-8 pt-14 shadow-2xl sm:px-6 sm:pt-8 md:p-6 lg:p-8 rounded-xl w-3/4 ${isAnswered ? 'answered' : null}`}>
             {
-                query.question ?
+                currentQuestion <= 5 ?
             <div className="block">
                 <h1 className="text-black text-4xl text-center p-5">{question}</h1>
                 <section className="grid grid-rows-2 grid-flow-col gap-2 p-5">
-                    {answers.map((answer, index) => {
+                    {possibleAnswers.map((answer, index) => {
                         return <div key={index}>
                             <div className={`h-[1rem]`}>
                                 {
                                 chosen === answer && showChosen?
-                                    <h1 className='text-black text-sm'>{matchedName} chose...</h1>
+                                    <h1 className='text-black text-sm'>{matched.currentUser.name} chose...</h1>
                                         :
                                         isClicked === answer ? 
                                             <h1 className='text-black text-sm'>You chose...</h1>
@@ -53,11 +74,22 @@ export default function Quiz({ query, setCurrentQuestion, setAnswers, matchedNam
                 }
                 </section>
                     </div> : 
-                <button className='p-2 bg-indigo-400 block w-1/4 mx-auto rounded'>Submit</button>
+                    <div className='w-3/4 mx-auto block space-y-3'>
+                        <h1 className='w-1/2 block mx-auto text-4xl text-black'>Your Answers</h1>
+                        {
+                            questions.map((question, index) => {
+                                const { chosen } = answers[index];
+                                return <div className=' space-x-3 mx-auto flex mb-3' key={index}>
+                                    <p className="text-black text-xl">{question}</p>
+                                     <p className="text-black text-xl">{chosen}</p> 
+                                    </div>
+                            })
+                        }
+                        <button className='p-2 bg-indigo-400 block w-1/4 mx-auto rounded'
+                        onClick={() => handleSubmit()}
+                            >Submit</button>
+                        </div>
             }
         </div>
     )
 };
-
-finish submit button on quiz submit
-build compatibility component
