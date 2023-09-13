3078a0bca9b007b169e6d2a3d3512d57e539df2f
Dakota King <86220170+loribaltimore@users.noreply.github.com>
2023-09-13 16:26:47 -0700

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -2,14 +2,13 @@ import {NextResponse} from 'next/server';
 import { headers } from 'next/headers';
 
 export async function middleware(request) {
-    let url;
-    process.env.NODE_ENV === 'development' ? url = 'localhost:3000' : 'datr-lyrat.vercel.app';
+    console.log(process.env.LOCAL_URL, 'URL IS');
     const cookie = headers().get('cookie') ?? '';
     const regex = /_next|\/api\/auth/g;
-    if (request.url !== `${url}/auth/signin`) {
-        if (request.url !== `${url}/api/auth/session`
+    if (request.url !== `${process.env.LOCAL_URL}/auth/signin`) {
+        if (request.url !== `${process.env.LOCAL_URL}/api/auth/session`
             && !request.url.match(regex)) {
-            const response = await fetch(`${url}/api/auth/session`, {
+            const response = await fetch(`${process.env.LOCAL_URL}/api/auth/session`, {
                 headers: {
                     cookie
                 },
@@ -24,11 +23,11 @@ export async function middleware(request) {
             if (response) {
                 return NextResponse.next();
             } else {
-                return NextResponse.redirect(`${url}/auth/signin`);
+                return NextResponse.redirect(`${process.env.LOCAL_URL}/auth/signin`);
             }
         } 
     } else {
-        const response = await fetch(`${url}/api/auth/session`, {
+        const response = await fetch(`${process.env.LOCAL_URL}/api/auth/session`, {
                 headers: {
                     cookie
                 },
@@ -41,7 +40,7 @@ export async function middleware(request) {
                 }
             }).catch(err => console.log(err));
         if (response) {
-            return NextResponse.redirect(`${url}/dashboard`)
+            return NextResponse.redirect(`${process.env.LOCAL_URL}/dashboard`)
         };
     }
 }
